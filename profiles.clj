60449b4fd94a32fa9d7fb68ad76271fcc25c7964
Anton Mostovoy <anton@mostovoy.net>
2016-04-13 16:06:51 -0500

    better repl reloading

---
+++ profiles.clj
@@ -0,0 +1,11 @@
+;see tools.namespace is to use improved refresh. see http://stackoverflow.com/a/25979645/2748570
+{:user {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
+        :plugins [[cider/cider-nrepl "0.8.2"]
+                  [venantius/ultra "0.3.3"]]
+        :ultra {:color-scheme :solarized_dark}
+        :source-paths [#=(eval (str (System/getProperty "user.home") "/.lein/src"))]
+        :repl-options
+          {:init-ns user
+           :init
+           (do
+           (set! *print-length* 100))}}}
---
+++ src/user.clj
@@ -0,0 +1,6 @@
+(ns user
+  (:require [clojure.tools.namespace.repl :refer [refresh]]))
+
+(import-vars
+  [clojure.tools.namespace.repl
+    refresh])
