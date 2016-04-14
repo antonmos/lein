2376dd43598e72ea414afe137d51cb42d8482400
Anton Mostovoy <anton@mostovoy.net>
2016-04-14 14:09:12 -0500

    make refresh callable via user/refresh

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,6 @@
 ;see tools.namespace is to use improved refresh. see http://stackoverflow.com/a/25979645/2748570
-{:user {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
+{:user {:dependencies [[org.clojure/tools.namespace "0.2.11"]
+                       [potemkin "0.4.3"]
         :plugins [[cider/cider-nrepl "0.8.2"]
                   [venantius/ultra "0.3.3"]]
         :ultra {:color-scheme :solarized_dark}
--- src/user.clj
+++ src/user.clj
@@ -1,5 +1,15 @@
 (ns user
-  (:require [clojure.tools.namespace.repl :refer [refresh]]))
+  (:require [potemkin :refer [import-vars]]
+            [clojure.main :refer [repl-requires]]
+            clojure.tools.namespace.repl))
+
+
+(doall (map (fn [[ns _ _]] (require ns)) repl-requires))
+
+(defn format-import [r]
+  (into [] (conj (last r) (first r))))
+  
+(eval (conj (map format-import repl-requires) 'import-vars))
 
 (import-vars
   [clojure.tools.namespace.repl
